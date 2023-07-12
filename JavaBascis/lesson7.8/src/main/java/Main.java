5814a554433c715db5cce8c2b3dd3cde47ed0d0a
Papageorgiou Nikolaos <nikolas_uoa@hotmail.com>
2023-07-12 19:06:16 +0300

    final_update
     To project αυτό δημιουργήθηκε με σκοπό να αναδείξει την σχέση έχει μεταξύ των
    κλάσεων κατασκευάσαμε μια κλάση Building πού έχει ένα πίνακα από Flats και στην συνέχεια κατασκευάσαμε μια κλάση Flat που διαθέτει ακέραιο αριθμό ανθρώπων.

--- JavaBascis/lesson7.8/src/main/java/Main.java
+++ JavaBascis/lesson7.8/src/main/java/Main.java
@@ -1,4 +1,7 @@
 public class Main {
+    //To project αυτό δημιουργήθηκε με σκοπό να αναδείξει την σχέση έχει μεταξύ των κλάσεων
+    //Κατασκευάσαμε μια κλάση Building πού έχει ένα πίνακα από Flats και στην συνέχεια
+    //Κατασκευάσαμε μια κλάση Flat που διαθέτει ακέραιο αριθμό αριθμό ανθρώπων.
     public static void main(String[] args) {
         Building b = new Building(3,4);
         b.addPeople(0,2,5);
